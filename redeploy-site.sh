0bbb032d2eee3d2c61a4de86e8337f9ab12c9f2a
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-07-22 11:50:32 -0500

    Update redeployment script with docker compose commands

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,6 +4,9 @@ cd portfolio
 
 git fetch && git reset origin/main --hard
 
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
+
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
